ff973e187188ac62b33245f124c4d796dc466814
Abhiram <81345491+Abhiram241@users.noreply.github.com>
2024-11-25 17:13:00 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,11 +2,21 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex bg-slate-500 justify-between py-3 ">
-      <div className="logo font-mono font-semibold text-xl mx-8 my-auto">NextUP</div>
+    <nav className="flex bg-gradient-to-r from-teal-400 to-indigo-600 justify-between py-3 ">
+      <div className="logo select-none cursor-pointer font-mono font-semibold text-xl mx-8 my-auto">
+        NextUP
+      </div>
       <ul className="flex gap-8 mx-5">
-        <li className="hover:font-bold transition-all cursor-pointer font-mono font-thin border-2 p-2">Home</li>
-        <li className="hover:font-bold transition-all cursor-pointer font-mono font-thin border-2 p-2">About</li>
+        <li className=" flex gap-2 items-center bg-gradient-to-r from-red-600 to-red-400 transition-all duration-300 hover:scale-110 cursor-pointer font-mono font-thin border-2 p-4 rounded-lg text-white">
+        <img src="./linkedin.svg" alt="" className="w-4 y-4 filter invert saturate-0" />
+
+          LinkedIn
+        </li>
+
+        <li className=" flex gap-2 items-center bg-gradient-to-r from-green-600 to-green-400 transition-all duration-300 hover:scale-110 cursor-pointer font-mono font-thin border-2 p-4 rounded-lg text-white">
+          <img src="./github.svg" alt="" className="w-4 y-4 filter invert saturate-0" />
+          Github
+        </li>
       </ul>
     </nav>
   );
