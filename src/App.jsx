5f9ac3a18f88f524a56b17e39719b2793b592408
Abhiram <81345491+Abhiram241@users.noreply.github.com>
2024-11-26 22:30:14 +0530

    some theming

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,47 @@ import Navbar from "./Components/Navbar";
 function App() {
   return (
     <>
-      
-        <Navbar/>
-    </>
+      <div className="bg-gradient-to-r from-gray-900 to-black min-h-[100vh]">
+      <Navbar />
+      <div
+        className="container mx-auto mt-20 my-5 rounded-xl p-5 w-1/2  "
+        style={{ backgroundColor: "#C8D5B9" }}
+      >
+        
+        <div className="addtodo">
+          <h2 className="text-3xl  font-bold text-center ">Plan Your Day</h2>
+          <div className="flex items-center justify-center mt-5">
+            <input
+              type="text"
+              name=""
+              className=" transition-all duration-100 hover:scale-110 h-12 w-[80%] rounded-l-xl p-2  mx-0 border border-gray-300 placeholder-gray-500 placeholder:text-center"
+              id=""
+              placeholder="Enter your todo"
+            />
+            <button
+              type="submit"
+              className="rounded-r-xl w-25 mx-0  p-2 h-12 flex items-center transition-all duration-100 hover:scale-125"
+              style={{ backgroundColor: "#FE654F" }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+
+        <h2 className="text-lg font-mono font-semibold">Your Todos</h2>
+        <div className="todos">
+          <div className="todo flex">
+            <div className="text">Lorem ipsum dolor sit.</div>
+            <div className="buttons">
+              <button className=""><img src="./edit.svg" alt="" /></button>
+              <button><img src="./delete.svg" alt="" /></button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      </div>
+          </>
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,21 +2,32 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex bg-gradient-to-r from-teal-400 to-indigo-600 justify-between py-3 ">
-      <div className="logo select-none cursor-pointer font-mono font-semibold text-xl mx-8 my-auto">
+    <nav
+      className="flex bg-gradient-to-r from-gray-800 to-gray-900
+ justify-between py-3 "
+    >
+      <h1 className="logo flex  gap-3 select-none p-3 text-orange-500  cursor-pointer font-mono font-extrabold  text-xl mx-8 my-auto">
+        <img src="./appicon.svg" alt="" className="" />
         NextUP
-      </div>
-      <ul className="flex gap-8 mx-5">
-        <li className=" flex gap-2 items-center bg-gradient-to-r from-red-600 to-red-400 transition-all duration-300 hover:scale-110 cursor-pointer font-mono font-thin border-2 p-4 rounded-lg text-white">
-        <img src="./linkedin.svg" alt="" className="w-4 y-4 filter invert saturate-0" />
+      </h1>
 
-          LinkedIn
+      <ul className="flex items-center gap-8 mx-5 my-0">
+        <li className=" h-10 flex gap-2 items-center bg-gradient-to-r from-red-600 to-red-400 transition-all duration-300 hover:scale-110 cursor-pointer font-mono font-thin border-2 p-4 rounded-lg text-white">
+          <img
+            src="./linkedin.svg"
+            alt=""
+            className="w-4 y-4 filter invert saturate-0"
+          />
+          <div className="text mt-1">LinkedIn</div>
         </li>
 
-        <li className=" flex gap-2 items-center bg-gradient-to-r from-green-600 to-green-400 transition-all duration-300 hover:scale-110 cursor-pointer font-mono font-thin border-2 p-4 rounded-lg text-white">
-          <img src="./github.svg" alt="" className="w-4 y-4 filter invert saturate-0" />
-          Github
-        </li>
+        <li className=" h-10 flex gap-2 items-center bg-gradient-to-r from-green-600 to-green-400 transition-all duration-300 hover:scale-110 cursor-pointer font-mono font-thin border-2 p-4 rounded-lg text-white">
+          <img
+            src="./github.svg"
+            alt=""
+            className="w-4 y-4 filter invert saturate-0"
+          />
+<div className="text mt-1">Github</div>        </li>
       </ul>
     </nav>
   );
